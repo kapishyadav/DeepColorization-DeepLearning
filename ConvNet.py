ba4f6a519ad0ab04cbfef75bab9362e46c0b0511
Nick Kroeger <kroegern1@live.com>
2020-11-06 20:21:44 -0500

    edited conv net arch

--- ConvNet.py
+++ ConvNet.py
@@ -9,40 +9,43 @@ class ConvNet(Module):
     def __init__(self):
         super(ConvNet, self).__init__()
 
-        self.cnn_layers = Sequential(
-            # Defining a 2D convolution layer 128*128
-            Conv2d(1, 3, kernel_size=8, stride=2, padding=3),
-            ReLU(inplace=True),
+        # Defining a 2D convolution layer 128*128
+        self.conv1 = Conv2d(1, 3, kernel_size=8, stride=2, padding=3)
+        self.relu1 = ReLU(inplace=True)
 
-            # Defining another 2D convolution layer 64*64
-            Conv2d(3, 3, kernel_size=8, stride=2, padding=3),
-            ReLU(inplace=True),
+        # Defining another 2D convolution layer 64*64
+        self.conv2 = Conv2d(3, 3, kernel_size=8, stride=2, padding=3)
+        self.relu2 = ReLU(inplace=True)
 
-            # Defining another 2D convolution layer 32*32
-            Conv2d(3, 3, kernel_size=4, stride=2, padding=1),
-            ReLU(inplace=True),
+        # Defining another 2D convolution layer 32*32
+        self.conv3 = Conv2d(3, 3, kernel_size=4, stride=2, padding=1)
+        self.relu3 = ReLU(inplace=True)
 
-            # Defining another 2D convolution layer 16*16
-            Conv2d(3, 3, kernel_size=4, stride=2, padding=1),
-            ReLU(inplace=True),
-            
-            # Defining another 2D convolution layer 8*8
-            Conv2d(3, 3, kernel_size=3, stride=3, padding=1),
-            ReLU(inplace=True),
-           
-            # Defining another 2D convolution layer 4*4
-            Conv2d(3, 3, kernel_size=2, stride=2),
-            ReLU(inplace=True),
-           
-            # Defining another 2D convolution layer 2*2
-            Conv2d(3, 2, kernel_size=1, stride=1),
-            ReLU(inplace=True),
-            
-        )
+        # Defining another 2D convolution layer 16*16
+        self.conv4 = Conv2d(3, 3, kernel_size=4, stride=2, padding=1)
+        self.relu4 = ReLU(inplace=True)
+
+        # Defining another 2D convolution layer 8*8
+        self.conv5 = Conv2d(3, 3, kernel_size=3, stride=3, padding=2)
+        self.relu5 = ReLU(inplace=True)
+
+        # Defining another 2D convolution layer 4*4
+        self.conv6 = Conv2d(3, 3, kernel_size=2, stride=2)
+        self.relu6 = ReLU(inplace=True)
+
+        # Defining another 2D convolution layer 2*2
+        self.conv7 = Conv2d(3, 2, kernel_size=2, stride=1)
+        self.relu7 = ReLU(inplace=True)
 
 
-
-    # Defining the forward pass    
+    # Defining the forward pass
     def forward(self, x):
-        x = self.cnn_layers(x)
-        return x
+        # x = self.cnn_layers(x)
+        x1 = self.relu1(self.conv1(x))
+        x2 = self.relu2(self.conv2(x1))
+        x3 = self.relu3(self.conv3(x2))
+        x4 = self.relu4(self.conv4(x3))
+        x5 = self.relu5(self.conv5(x4))
+        x6 = self.relu6(self.conv6(x5))
+        x7 = self.relu7(self.conv7(x6))
+        return x7
